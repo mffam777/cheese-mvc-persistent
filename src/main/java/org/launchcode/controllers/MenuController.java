5cfdc208f870d73c9ee872ceed9ddc15dc843629
AM <mffam777@gmail.com>
2019-03-19 11:04:37 -0400

    Completed MySQL server set up and added Category Class to model with requires constructors.
    Completed Category Controller, Dao, Index and Add html.
    
    Added @OneToMany to Category
    
    Added Menu; model, controller, Dao, and templates
    Completed forms
    
    Work in menuController with the add-item

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -31,6 +28,7 @@ public class MenuController {
     // Request path: /menu
     @RequestMapping(value = "")
     public String index(Model model) {
+        System.out.println("Index - Menu Cont");
         model.addAttribute("title" , "Menus");
         model.addAttribute("menus" , menuDao.findAll());
         return "menu/index";
@@ -39,6 +37,8 @@ public class MenuController {
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add(Model model) {
+        System.out.println("Add - GET - Menu Cont");
+
         model.addAttribute("title" , "Add Menu");
         model.addAttribute(new Menu());
         return "menu/add";
@@ -47,6 +47,7 @@ public class MenuController {
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String add(Model model , @ModelAttribute @Valid Menu menu ,
                       Errors errors) {
+        System.out.println("Add - POST - Menu Cont");
 
         if (errors.hasErrors()) {
             model.addAttribute("title" , "Add Menu");
@@ -54,16 +55,18 @@ public class MenuController {
         }
 
         menuDao.save(menu);
-        return "redirect:view/" + menu.getId();
+        return "redirect:/menu/view/" + menu.getId();
     }
 
     @RequestMapping(value = "view/{menuId}", method = RequestMethod.GET)
     public String viewMenu(Model model , @PathVariable int menuId) {
+        System.out.println("View/MenuID - GET - Menu Cont");
 
         Menu menu = menuDao.findOne(menuId);
         model.addAttribute("title" , menu.getName());
         model.addAttribute("cheeses" , menu.getCheeses());
         model.addAttribute("menuId" , menu.getId());
+        model.addAttribute("menu", menu);
 
         return "menu/view";
 
@@ -71,27 +74,28 @@ public class MenuController {
 
     @RequestMapping(value = "add-item/{menuId}", method = RequestMethod.GET)
     public String addItem(Model model , @PathVariable int menuId) {
-        System.out.println("Add Item");
+        System.out.println("Add Item/menu ID - Get - MENU Controller");
         Menu menu = menuDao.findOne(menuId);
 
-        AddMenuItemForm form = new AddMenuItemForm(
-                cheeseDao.findAll(),
+        AddMenuItemForm form = new AddMenuItemForm(cheeseDao.findAll(),
                 menu);
         List<Cheese> cheeses = menu.getCheeses();
 
         model.addAttribute("title" , "Add item to menu: " + menu.getName());
         model.addAttribute("form" , form);
         model.addAttribute("cheeses" , menu.getCheeses());
+
         return "menu/add-item";
     }
 
+    // NOt Working
     @RequestMapping(value = "add-item", method = RequestMethod.POST)
-    public String addItem(Model model ,
-                          @ModelAttribute @Valid AddMenuItemForm form,
-                          Errors errors) {
-
+    public String addItem(@ModelAttribute @Valid AddMenuItemForm form, Errors errors, Model model) {
+        System.out.println("Add-Item - MEnu Controller -POST");
         if (errors.hasErrors()) {
+
             model.addAttribute("form" , form);
+
             return "menu/add-item";
         }
 
